3505c965e8124eca44929e146dfb7300cd3fe6aa
thotheod <thotheod@outlook.com>
2023-01-12 15:15:02 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Kubernetes Service (AKS) workshop
+        Azure Redhat Openshift (ARO) workshop
       </div>
       <div class="col-lg-6">
       </div>
